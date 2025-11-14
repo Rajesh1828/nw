eef18b51e521839062792ed9e924792a1f89f249
Rajesh18 <161973375+Rajesh1828@users.noreply.github.com>
2025-11-14 17:35:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://natural-demo-18x9.vercel.app",
     credentials: true,
   })
 );
